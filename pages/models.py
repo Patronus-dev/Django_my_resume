d19a2e91c73c09781d8f6233c032ebf4effa7e3a
Mehdi <mehdii.dev@gmail.com>
2024-11-24 21:51:53 +0330

    template edited

--- pages/models.py
+++ pages/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 
 class ContactMessage(models.Model):
-    name = models.CharField(max_length=50, blank=False, null=False, verbose_name='Name')
-    email = models.EmailField(blank=False, null=False, verbose_name='Email')
-    subject = models.CharField(max_length=100, blank=True, verbose_name='Subject')
-    message = models.TextField(max_length=500, blank=False, null=False, verbose_name='Message')
+    name = models.CharField(max_length=50, blank=False, null=False, verbose_name=_('Name'))
+    email = models.EmailField(blank=False, null=False, verbose_name=_('Email'))
+    subject = models.CharField(max_length=100, blank=True, verbose_name=_('Subject'))
+    message = models.TextField(max_length=500, blank=False, null=False, verbose_name=_('Message'))
     datetime_creation = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
