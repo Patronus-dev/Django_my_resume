f42fe6c35d589be4fbc28c6303f85b65b29204ed
Patronus-dev <mehdii.dev@gmail.com>
2024-12-22 18:35:54 +0330

    project deployed

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,6 @@ env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -18,7 +17,7 @@ SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['patronuscode.com', 'www.patronuscode.com']
 
 SITE_ID = 1
 
@@ -86,8 +85,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env.str("NAME"),
+#         'USER': env.str("USER"),
+#         'PASSWORD': env.str("PASSWORD"),
+#         'HOST': env.str("HOST", default="localhost"),
+#         'PORT': env.str("PORT", default="5432"),
+#     }
+# }
 DATABASES = {
-     'default': env.db('DATABASE_URL')
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'patronus_database',
+        'USER': 'patronus_userdatabase',
+        'PASSWORD': 'M12345patronusdatabase@',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 
@@ -134,13 +150,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'assets/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'), ]
+# STATIC_URL = '/assets/'
+# STATIC_ROOT = '/home/patronus/public_html/assets/'
+
+STATIC_URL = '/assets/'
+STATIC_ROOT = '/home/patronus/public_html/assets/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles',
+]
+
 
 # Media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+MEDIA_ROOT = '/home/patronus/public_html/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -177,3 +199,4 @@ MAINTENANCE_MODE_TEMPLATE = "503.html"
 
 # Custom error handler 404 error
 HANDLER404 = 'pages.views.custom_404_view'
+
